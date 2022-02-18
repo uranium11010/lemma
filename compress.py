1edae73cae89f046331ffbd7ef878d7094b5ab35
uranium11010 <zed.zhening.li@hotmail.com>
2022-02-18 12:41:21 -0500

    Added abstracting sol; organized into classes
    Former-commit-id: fabd25b749ecd90ecb0991c9d562be0c264907ed

--- compress.py
+++ compress.py
@@ -10,81 +10,149 @@ import warnings
 import util
 
 
-def get_frequencies(solutions, num_ax, axioms, get_ax_name):
-    """
-    Gets frequencies of (current action, next action) pairs
-    """
-    frequencies = np.zeros((num_ax, num_ax), dtype=int)
-    axiom_index = {axioms[k]: k for k in range(num_ax)}
-    for i in range(len(solutions)):
-        sol = solutions[i]["solution"]
-        for step in range(1, len(sol)-1):
-            action_cur = axiom_index[get_ax_name(sol[step]["action"])]
-            action_next = axiom_index[get_ax_name(sol[step+1]["action"])]
-            frequencies[action_cur, action_next] += 1
+class Compress(object):
+    def __init__(self, solutions, axioms, get_ax_name):
+        self.solutions = solutions
+        self.num_ax = len(axioms)
+        self.axioms = axioms
+        self.get_ax_name = get_ax_name
+        self.axiom_index = {self.axioms[k]: k for k in range(self.num_ax)}
     
-    return frequencies
+
+    def common_subseq(self):
+        raise NotImplementedError
+
+    
+    @staticmethod
+    def abstract_step(states, actions, abs_len, abstractions):
+        """
+        In solution, abstract out the first length-'abs_len' subsequence that is an abstraction
+        'actions' is tuple of integers denoting axioms in the solution
+        'states' is a list of states in the solution
+        """
+        for i in range(len(actions)-abs_len+1):
+            if actions[i:i+abs_len] in abstractions:
+                new_actions = actions[:i] + (-1,) + actions[i+abs_len:]
+                new_states = states[:i+1] + states[i+abs_len:]
+                return new_states, new_actions
+
+    
+    def abstracted_sol(self, max_len, abstractions=None):
+        """
+        Get abstracted solutions from set of abstractions
+        """
+        if abstractions is None:
+            abstractions = self.common_subseq()
+        
+        new_sols = [([self.solutions[i]["solution"][step]["state"] for step in range(len(self.solutions[i]["solution"]))],
+                    tuple(self.axiom_index[self.get_ax_name(self.solutions[i]["solution"][step]["action"])] for step in range(1, len(self.solutions[i]["solution"]))))
+                             for i in range(len(self.solutions))]
+        for i in range(len(new_sols)):
+            new_sol = new_sols[i]
+            # print("BEGIN", new_sol[1])
+            for abs_len in range(max_len, 1, -1):
+                while True:
+                    res = CommonPairs.abstract_step(*new_sol, abs_len, abstractions)
+                    if res is None:
+                        break
+                    else:
+                        new_sol = res
+                        # print("ABS_LEN", abs_len, new_sol[1])
+            
+            new_sols[i] = new_sol
+        
+        return new_sols
 
 
-def dfs(N, graph, cur, paths, visited, cur_paths):
-    """
-    Finds all paths in graph starting at 'cur' node
-    Adds them to 'paths' (or augments existing paths)
-    Indices of paths in 'paths' that 'cur' node belongs to are in 'cur_paths'
-    Nodes in paths to that led to 'cur' node are in 'visited'
-    ATTENTION: NOT YET OPTIMIZED FOR WHEN MANY PATHS GO TO SAME NODE (i.e. dfs would be repeatedly called on that node)
-    """
-    copied_cur_paths = [paths[path_idx].copy() for path_idx in cur_paths]
-    first_next = True
-    for next in range(N):
-        if graph[cur, next]:
-            if first_next:
-                for path_idx in cur_paths:
-                    paths[path_idx].append(next)
-                next_paths = cur_paths
-                first_next = False
-            else:
+
+
+class CommonPairs(Compress):
+    def __init__(self, solutions, axioms, get_ax_name, thres=None):
+        super().__init__(solutions, axioms, get_ax_name)
+        self.thres = thres
+
+    def get_frequencies(self):
+        """
+        Gets frequencies of (current action, next action) pairs
+        """
+        frequencies = np.zeros((self.num_ax, self.num_ax), dtype=int)
+        for i in range(len(self.solutions)):
+            sol = self.solutions[i]["solution"]
+            for step in range(1, len(sol)-1):
+                action_cur = self.axiom_index[get_ax_name(sol[step]["action"])]
+                action_next = self.axiom_index[get_ax_name(sol[step+1]["action"])]
+                frequencies[action_cur, action_next] += 1
+        
+        return frequencies
+
+
+    @staticmethod
+    def dfs(N, graph, cur, paths, visited, cur_paths):
+        """
+        Finds all paths in graph starting at 'cur' node
+        Adds them to 'paths' (or augments existing paths)
+        Indices of paths in 'paths' that 'cur' node belongs to are in 'cur_paths'
+        Nodes in paths that led to 'cur' node are in 'visited'
+        ATTENTION: NOT YET OPTIMIZED FOR WHEN MANY PATHS GO TO SAME NODE (i.e. dfs would be repeatedly called on that node)
+        """
+        copied_cur_paths = [paths[path_idx] for path_idx in cur_paths]
+        # first_next = True
+        for next in range(N):
+            if graph[cur, next]:
+                # COMMENT OUT THIS IF STATEMENT IF YOU WANT PATHS LIKE [5, 8, 5] INSEAD OF [5, 8] FOR CYCLES
+                if next in visited:
+                    warnings.warn("Note: the graph has cycles")
+                    continue
+
+                # if first_next:
+                #     for path_idx in cur_paths:
+                #         paths[path_idx] = paths[path_idx] + (next,)
+                #     next_paths = cur_paths
+                #     first_next = False
+                # else:
                 next_paths = []
                 for cur_path in copied_cur_paths:
-                    paths.append(cur_path + [next])
+                    paths.append(cur_path + (next,))
                     next_paths.append(len(paths)-1)
-            
-            if next in visited:
-                warnings.warn("Cool fact--the graph has cycles :)")
-                continue
-            
-            new_visited = visited.copy()
-            new_visited.add(next)
-            dfs(N, graph, next, paths, new_visited, next_paths)
+                
+                # UNCOMMENT THIS PART IF YOU WANT PATHS LIKE [5, 8, 5] INSEAD OF [5, 8] FOR CYCLES
+                # if next in visited:
+                #     warnings.warn("Note: the graph has cycles")
+                #     continue
+                
+                new_visited = visited.copy()
+                new_visited.add(next)
+                CommonPairs.dfs(N, graph, next, paths, new_visited, next_paths)
 
 
-def maximal_paths(N, graph):
-    """
-    Helper for common_subseq(..)
-    Finds maximal paths in directed graph given by adjacency matrix 'graph'; N = # nodes
-    ATTENTION: NOT 'MAXIMAL' IN THE REVERSE DIRECTION (SO IF [2, 5, 3, 5] IS DETECTED, [5, 3, 5] ALSO WOULD BE),
-    HENCE THERE ARE MANY REDUNDANCIES
-    """
-    paths = []
-    for node in range(N):
-        paths.append([node])
-        dfs(N, graph, node, paths, {node}, [len(paths)-1])
-    
-    return paths
+    @staticmethod
+    def maximal_paths(N, graph):
+        """
+        Helper for common_subseq(..)
+        Finds maximal paths in directed graph given by adjacency matrix 'graph'; N = # nodes
+        ATTENTION: NOT 'MAXIMAL' IN THE REVERSE DIRECTION (SO IF [2, 5, 3, 5] IS DETECTED, [5, 3, 5] ALSO WOULD BE),
+        HENCE THERE ARE MANY REDUNDANCIES
+        """
+        paths = []
+        for node in range(N):
+            paths.append((node,))
+            CommonPairs.dfs(N, graph, node, paths, {node}, [len(paths)-1])
+        
+        return set(paths)
 
 
-def common_subseq(solutions, num_ax, axioms, get_ax_name, thres=None):
-    """
-    Finds common subsequences among solutions where any (current action, next action)
-    pair within subsequence appears a fruency >= thres in dataset of solutions
-    """
-    thres = num_ax**(-0.75) if thres is None else thres # -0.75 is just an arbitrary number between -1 and 0 that I chose
-    thres = int(np.ceil(len(solutions) * thres))
-    graph = get_frequencies(solutions, num_ax, axioms, get_ax_name) >= thres
+    def common_subseq(self):
+        """
+        Finds common subsequences among solutions where any (current action, next action)
+        pair within subsequence appears a fruency >= thres in dataset of solutions
+        """
+        thres = self.num_ax**(-0.75) if self.thres is None else self.thres # -0.75 is just an arbitrary number between -1 and 0 that I chose
+        thres = int(np.ceil(len(solutions) * thres))
+        graph = self.get_frequencies() >= thres
 
-    print(graph.astype(int))
-    util.draw_graph(num_ax, graph)
-    return maximal_paths(num_ax, graph)
+        print(graph.astype(int))
+        util.draw_graph(self.num_ax, graph)
+        return CommonPairs.maximal_paths(self.num_ax, graph)
 
 
 def get_ax_name(ax_full):
@@ -96,7 +164,7 @@ def get_ax_name(ax_full):
 
 if __name__ == "__main__":
     solutions = util.load_solutions("equations-8k.json")
-    num_ax, axioms = util.load_axioms("equation_axioms.json")
+    _, axioms = util.load_axioms("equation_axioms.json")
 
     # freq = get_frequencies(solutions, num_ax, axioms, get_ax_name)
     # print(freq)
@@ -116,6 +184,15 @@ if __name__ == "__main__":
     # print(paths)
     # print(maximal_paths(6, graph))
 
-    abstractions = common_subseq(solutions, num_ax, axioms, get_ax_name)
-    print(abstractions)
+    compressor = CommonPairs(solutions, axioms, get_ax_name)
+    abstractions = compressor.common_subseq()
+    # print(abstractions)
+    abs_sol = compressor.abstracted_sol(5, abstractions=abstractions)
+    ex_sol = abs_sol[59]
+    print("State 0:", ex_sol[0][0])
+    for i in range(1, len(ex_sol[0])):
+        ax_idx = ex_sol[1][i-1]
+        ax = axioms[ax_idx] if ax_idx >= 0 else "ABSTRACTION"
+        print("Action {}:".format(i), ax)
+        print("State {}:".format(i), ex_sol[0][i])
 
